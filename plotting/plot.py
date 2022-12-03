c41c0dd3a2c3ce1f0c9c808decfab519e27ceb41
yifan36 <yifanma1106@gmail.com>
2022-12-03 00:07:12 -0500

    Update plot.py

--- plotting/plot.py
+++ plotting/plot.py
@@ -12,15 +12,29 @@ import matplotlib.pyplot as plt
 
 #%% 
 
+def load_trace_file(filename):
+    raise NotImplementedError("")
 
+def calc_qrtd(trace):
+    raise NotImplementedError("")
+
+def calc_sqd(trace):
+    raise NotImplementedError("")
+
+def plot_boxplot(trace):
+    raise NotImplementedError("")
+
+
+def plot_qrtd(qrtd):
+    raise NotImplementedError("")
+
+def plot_sqd(sqd):
+    raise NotImplementedError("")
+
+#%%
 # import trace file from different runs 
 filename=""
 trace=load_trace_file(filename)
-
-
-
-
-
 
 
 # calculation
@@ -28,20 +42,12 @@ trace=load_trace_file(filename)
 # Qualified Runtime for various solution qualities
 
 qrtd=calc_qrtd(trace)
-
 # Solution Quality Distributions for various run-times
 
-
-
-
-
+sqd=calc_sqd(trace)
 
 
 # plot 
-
-
-def load_trace_file(filename):
-    pass
-
-def calc_qrtd(trace):
-    raise Notimp
+plot_qrtd(qrtd)
+plot_sqd(sqd)
+plot_boxplot(trace)
