2dcf6c74ca5003e1d07b15bb03d9557a0df839cd
yifan36 <yifanma1106@gmail.com>
2022-12-03 00:04:47 -0500

    Plotting script
    Only a skeleton

---
+++ plotting/plot.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 23:26:46 2022
+
+@author: yifan
+"""
+
+import time 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% 
+
+
+# import trace file from different runs 
+filename=""
+trace=load_trace_file(filename)
+
+
+
+
+
+
+
+# calculation
+
+# Qualified Runtime for various solution qualities
+
+qrtd=calc_qrtd(trace)
+
+# Solution Quality Distributions for various run-times
+
+
+
+
+
+
+
+# plot 
+
+
+def load_trace_file(filename):
+    pass
+
+def calc_qrtd(trace):
+    raise Notimp
