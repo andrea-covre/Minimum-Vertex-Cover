08032c3c5a2ad2681942b4c49b88e8d6660ba41d
zincarrot <zincarrot@gmail.com>
2022-12-04 00:40:23 -0500

    Create run_custom.bat

---
+++ run_custom.bat
@@ -0,0 +1,17 @@
+cd %~dp0
+@echo off
+echo select graph, algorithm and repeat times
+set /p g= graph: star2/power(s/p): 
+set /p a= algo: LS!1/LS2(1/2): 
+set /p n= repeat #:
+set /p t= time: 
+if %g%==s set graph=./data/star2.graph
+if %g%==p set graph=./data/power.graph
+if %a%==1 set algo=LS1
+if %a%==2 set algo=LS2
+
+set /a i=0
+:loop1
+python -m exec -inst %graph% -alg %algo% -time %t% -seed %Random%
+set /a i=%i%+1
+if %i% neq %n% goto loop1
