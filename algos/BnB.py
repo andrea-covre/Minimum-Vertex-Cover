54de7e77b11cd40dac40a335dc97f4e563f0dbd5
Snowleaf Echo <jskjin42@gmail.com>
2022-12-04 12:03:31 -0500

    Remove trivial approx call; remove comments

--- algos/BnB.py
+++ algos/BnB.py
@@ -1,9 +1,9 @@
 """
 This file contains the logic for the Branch and Bound algorithm.
 """
-from algos.Approx import Approx
+#from algos.Approx import Approx
 from copy import deepcopy
-import networkx as nx
+#import networkx as nx
 from typing import Tuple, List
 
 from graph import Graph
@@ -48,11 +48,12 @@ class BnB:
         Frontier = []
         neighbor = []
         
-        #initialize UB with Approx algo(max degree)
-        appr=Approx()
-        UpperBound, solution_appr = appr.get_vertex_cover(self.G, self.timer, self.trace)
-        UpperBound += 1
+        #initialize UB
+        #appr=Approx()
+        #UpperBound, solution_appr = appr.get_vertex_cover(self.G, self.timer, self.trace)
+        #UpperBound += 1
         #initialize new graph to be divided in subproblems
+        UpperBound = G.v
         CurG = deepcopy(G)
         v = find_maxdeg(CurG)
         
@@ -202,26 +203,3 @@ def VC_Size(VC):
         if VC[i][1] == 1:
             vc_size += 1;
     return vc_size
-    
- ######################
- ### YOUR CODE HERE ###
- ######################
- # ===== Useful info: =====
- # > feel free to import the random module if needed, but do not worry about setting the seed as it is already set globally in exec.py
- # > G.v is the number of nodes in the graph
- # > G.e is the number of edges in the graph
- # > G.get_neighbours(node) returns the list of neighbours of the given node (use this API to access the graph, so that the accesses count is updated)
- # > G.get_all_nodes() returns the list of all nodes in the graph
- # > G.check_vertex_cover(vertex_cover) returns the number of vertexes covered by the vertex cover (use this API, so that the vertex cover checks count is updated)
- # > the nodes in the graph are numbered from 1 to G.v
- # > use timer.cutoff() to check if the time limit has been exceeded and you need to stop the algorithm
- # > use trace.add_record(quality) to add a new record to the trace, do not worry about the timestamp (as it is added automatically) or about saving the file 
- # > return the best solution found (as a list of nodes) and its quality (number of edges covered)
- #
- # >>> if you need anything feel free to let Andrea know! <<<
- #
- ######################
- 
- #raise NotImplementedError("branch_and_bound in algos/BnB.py not implemented yet")
- 
- # remove redundant nodes
