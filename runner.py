1c2efea657ffb50ae40f54dbffea009db7fe7c6e
andrea-covre <85234549+andrea-covre@users.noreply.github.com>
2022-12-03 20:36:41 -0500

    added path option to runner.py

--- runner.py
+++ runner.py
@@ -10,33 +10,36 @@ import os
 import argparse
 from exec import ALGOS, print_args
 
-__GRAPHS_DIR__ = 'data'
-
 def parse_args():
     """ Parses the arguments passed to the script """
     
     # Creating the parser
     parser = argparse.ArgumentParser(description='Run all the algorithms on all datasets')
     
+    parser.add_argument('-inst', type=str, help='directory with the graphs to be used', required=True)
     parser.add_argument('-time', type=int, help='cutoff time in seconds', required=True)
     parser.add_argument('-seed', type=int, help='random seed', required=True)
     
     return parser.parse_args()
 
 
-def get_graphs():
+def get_graphs(dir):
     """ Getting all graphs filenames """
-    items = os.listdir(__GRAPHS_DIR__)
+    items = os.listdir(dir)
     return [item for item in items if item.endswith('.graph')]
 
 
 def main():
     args = parse_args()
+    
+    if not os.path.exists(args.inst):
+        raise ValueError('Invalid directory')
+        
     print_args(args)
     
     for algo in ALGOS.keys():
-        for graph in get_graphs():
-            os.system(f"python -m exec -inst {__GRAPHS_DIR__}/{graph} -alg {algo} -time {args.time} -seed {args.seed}")
+        for graph in get_graphs(args.inst):
+            os.system(f"python -m exec -inst {os.path.join(args.inst, graph)} -alg {algo} -time {args.time} -seed {args.seed}")
     
 
 if __name__ == '__main__':
